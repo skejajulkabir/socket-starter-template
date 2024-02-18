9aac1d204716014272f676cfa7104fef85d68f42
skejajulkabir <522ajaj@gmail.com>
2024-02-19 02:26:09 +0600

    commit before video calling functionality added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,32 +21,33 @@ function App() {
   useEffect(() => {
     socket.on("connect", () => {
       setSocketId(socket.id);
-      // console.log(socket)
       console.log("connected", socket.id);
     });
 
     socket.on("receive-message", (data) => {
-      console.log(data);
+      console.log("this is from server..." , data);
       setMessages((messages) => [...messages, data]);
     });
 
-    socket.on("welcome", (s) => {
-      console.log(s);
-    });
+    // socket.on("welcome", (s) => {
+    //   console.log(s);
+    // });
 
     return () => {
       // socket.disconnect();
     };
   }, []);
 
+  console.log(messages)
+
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    socket.emit("message", { message });
+    socket.emit("message", { message: message , room : room });
     // setMessage("");
   };
 
-  const joinRoomHandler = (e) => {
+  const handleSetRoom = (e) => {
     e.preventDefault();
     socket.emit("join-room", roomName);
     setRoomName("");
@@ -62,13 +63,45 @@ function App() {
             hello this is a socket tutorial...
           </h1>
 
+
+          <h4 className="">
+            Room Id: <span>{socketID}</span>
+          </h4>
+
           <hr />
 
-          <form onSubmit={handleSubmit}>
-            <input onChange={(e)=>setMessage(e.target.value)} value={message} type="text" placeholder='add message here...' />
+          <div className="">
+            <form onSubmit={handleSubmit}>
+              <input onChange={(e)=>setMessage(e.target.value)} value={message} type="text" placeholder='add message here...' />
+              <input onChange={(e)=>setRoom(e.target.value)} value={room} type="text" placeholder='set a room...' />
 
-            <button type='submit'>submit</button>
+              <button type='submit'>submit</button>
+            </form>
+          </div>
+
+          {/* <div className="">
+          <form onSubmit={handleSetRoom}>
+            <input onChange={(e)=>setRoom(e.target.value)} value={room} type="text" placeholder='set a room...' />
+
+            <button type='submit'>setroom</button>
           </form>
+          </div> */}
+
+          <div className="">
+            <div className="">
+              messages:
+            </div>
+
+            <ul>
+              {
+                messages.map((m , i)=>{
+                  return <li key={i}>{m.data}</li>
+                })
+              }
+            </ul>
+          </div>
+
+
         </div>
       </div>
     </>
--- server/index.js
+++ server/index.js
@@ -3,7 +3,21 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 const http = require("http");
 
+const PORT = 3000;
+
+
 const app = express();
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["*"], 
+    credentials: true,
+},
+});
 
 app.use(
   cors({
@@ -13,18 +27,6 @@ app.use(
   })
 );
 
-const server = http.createServer(app);
-
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["*"], 
-    credentials: true,
-  },
-});
-
-const PORT = 3000;
-
 app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
@@ -32,12 +34,29 @@ app.get("/", (req, res) => {
 io.on("connect", (socket) => {
   console.log("a user connected and the id is, ", socket.id);
 
+//   socket.emit("welcome", "welcome to server.")
+//   socket.broadcast.emit("welcome", `${socket.id} joined the server...`);
 
-  socket.on("message", (data) => { // Listen for "message" event on individual sockets
+  socket.on("message", (data) => {
     console.log(data);
-    io.emit("receive-message", data); // Emit to all connected sockets
+    // socket.broadcast.emit("receive-message", {status : "message paisi!" , data});
+    socket.broadcast.to(data.room).emit("receive-message", {status : "message paisi!" , data : data.message });
   });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+//   socket.on("join-room", (roomName)=>{
+//     socket.join(roomName);
+//     console.log(`user joined room ${roomName}`);
+//   })
+
+// console.log(socket)
+
 });
+
+
 
 
 
